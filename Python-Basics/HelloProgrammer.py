8723deb48f96e43bfe4d73b7d90bfbe09753308c
AnasimosM <anasimosmesmesfinkebede@gmail.com>
2026-08-31 00:49:08 -0700

    Update

---
+++ Python-Basics/HelloProgrammer.py
@@ -0,0 +1,54 @@
+# Education Material
+# Page 1
+
+print('\n-----------------\n')
+
+print('Hello Programmer!')
+
+print('\n-----------------\n')
+
+First_Name = 'John'
+LastName = 'Doe'
+
+print(First_Name + ' ' + LastName)
+
+print('\n-----------------\n')
+
+print('Hello Programmer! '+ First_Name + ' ' + LastName)
+
+print('\n-----------------\n')
+
+num1 = 5
+num2 = 3
+add = num1 + num2
+subtract = num1 - num2
+multiply = num1 * num2
+divide = num1 / num2
+
+print('Sum of ',num1,' and ',num2,' is ',add)
+print('Difference of ',num1,' and ',num2,' is ',subtract)
+print('Product of ',num1,' and ',num2,' is ',multiply)
+print('Quotient of ',num1,' and ',num2,' is ',divide)
+
+print('\n-----------------\n')
+
+# Make a command-line program that asks for a name,
+# age and goal, then prints a formatted profile
+
+name = input('Enter your name: \n')
+age = input('Enter your age: \n')
+goal = input('Enter your goal: \n')
+
+print('My name is ',name,'. I am ',age,'years old. My goal in life is ',goal)
+
+# print('\n-----------------\n')
+#
+#
+#
+# try:
+#     name = str(input('Enter your name: '))
+#     age = int(input('Enter your age: '))
+#     goal = str(input('Enter your goal: '))
+#     print(f'Hello {name}, you are {age} years old. My goal is {goal}')
+# except ValueError:
+#         print('Invalid Input')
