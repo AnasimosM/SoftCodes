4f9a62d994776c607f1f479ceb8add2f5a1579f3
AnasimosM <anasimosmesmesfinkebede@gmail.com>
2026-09-02 13:54:06 -0700

    Number Guesser (IF and While)

---
+++ Python-Basics/NumGuessIF.py
@@ -0,0 +1,41 @@
+# Conversion App
+# Page 3 LC3
+
+import random
+
+print("\t\t\t Number Guessing Game")
+print("\t 1. Start Game ")
+print("\t 2. Exit Game ")
+
+while True:
+    try:
+        opt = int(input("Enter your choice: "))
+        if opt == 1:
+            break
+        elif opt == 2:
+            exit()
+    except ValueError:
+        print("Please enter a number between 1 and 2")
+while True:
+    attempt = 0
+    while True:
+        gen = random.randint(1, 1000)
+        attempt += 1
+        try:
+
+            guess = int(input("Enter your guess: "))
+        except ValueError:
+            print("Please valid number")
+            continue
+        if guess < gen:
+            print("Your guess is too low")
+        elif guess > gen:
+            print("Your guess is too high")
+        elif gen == guess:
+            print(f"You guessed right! The number is {guess}")
+            print(f"Attempted {attempt} times")
+            break
+
+    again = str(input("Do you want to play again? (y/n): "))
+    if again != "y":
+        exit()
