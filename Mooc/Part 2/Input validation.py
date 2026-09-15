68727066d2cc4d58577c10d998da4a9a57be71c2
AnasimosM <322534541+AnasimosM@users.noreply.github.com>
2026-09-14 17:42:54 -0700

    Updated

--- Mooc/Part 2/Input validation.py
+++ Mooc/Part 2/Input validation.py
@@ -6,7 +6,7 @@ while True:
     if number == 0:
         print("Exiting...")
         break
-    elif number < 0:
+    elif number < 0: 
         print("Invalid number")
     else:
         print(sqrt(number))
