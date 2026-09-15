5f681e3f7e67c61df4de279cf0f4d3c5ca2dd83e
AnasimosM <anasimosmesmesfinkebede@gmail.com>
2026-09-15 01:54:28 -0700

    Update PIN Check program to track and display all user attempts

--- Mooc/Part 2/PIN Check.py
+++ Mooc/Part 2/PIN Check.py
@@ -1,15 +1,18 @@
 PIN = 4321
 attempts = 0
-
+words = ""
 while True:  # attempts != 0
     user_input = int(input("PIN: "))
     attempts += 1
+    words += str(user_input) + ", "
     if user_input == PIN:
         if attempts > 1:
-            print(f"Correct! It took you {attempts} attempts")  
+            print(f"Correct! It took you {attempts} attempts")
+            print(words)
             break
         else:
             print("Correct! It only took you one single attempt!")
+            print(words)
             break
     print("Wrong")
 
