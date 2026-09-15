095ff4f4bb301493d51944c4ab32e0f1f840d0f1
AnasimosM <322534541+AnasimosM@users.noreply.github.com>
2026-09-14 17:45:06 -0700

    Updated

--- Mooc/Part 2/Shall we continue.py
+++ Mooc/Part 2/Shall we continue.py
@@ -2,6 +2,6 @@ while True:
     print(f"hi")
     choice = input("Shall we continue?:")
 
-    if choice == "no" or choice == "No":
+    if choice == "no" or choice == "No": 
         break
 print(f"okay then")
