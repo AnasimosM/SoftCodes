d118c7afd4253f969e69a6f3edd79414de95a140
AnasimosM <anasimosmesmesfinkebede@gmail.com>
2026-09-15 13:22:38 -0700

    Add Numbers program to count from 1 up to a user-defined limit in Part 3 of Mooc

---
+++ Mooc/Part 3/Numbers.py
@@ -0,0 +1,10 @@
+limit = int(input("Upper limit: "))
+number = 1
+
+while 0 < number < limit:
+    print(number)
+    number += 1
+
+# while 0 < limit > limit - 1:
+#     print(limit)
+#     limit -= 1
