c8f2b95e363d18e1fd9e425852c549e746084f2a
AnasimosM <anasimosmesmesfinkebede@gmail.com>
2026-09-15 03:07:11 -0700

    Add Story program to collect and display user input words

--- Mooc/Part 2/Story.py
+++ Mooc/Part 2/Story.py
@@ -3,7 +3,7 @@ last_word = ""
 while True:
     user_input = input("Please type in a word: ")
     user_input = user_input.replace(" ", "")
-    if user_input == "j" or user_input == last_word:
+    if user_input == "end" or user_input == last_word:
         break
     words += user_input + " "
     last_word = user_input
