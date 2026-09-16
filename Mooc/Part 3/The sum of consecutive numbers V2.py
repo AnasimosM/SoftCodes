969b46c5bbaa2b91356df7db9039892ced3ae5f5
AnasimosM <anasimosmesmesfinkebede@gmail.com>
2026-09-15 17:56:12 -0700

    Add revised program to calculate the sum of consecutive numbers up to a user-defined limit in Part 3 of Mooc

---
+++ Mooc/Part 3/The sum of consecutive numbers V2.py
@@ -0,0 +1,27 @@
+limit = int(input("Limit: "))
+number = 1
+current_value = 0
+display = ""
+while current_value < limit:
+    current_value += number
+    display += str(number) + " + "
+    number = number + 1
+
+print(f"The consecutive sum: {display[:-3]} = {current_value}")
+
+# limit = int(input("Limit: "))
+# number = 1
+# current_value = 0
+# display = "1"
+# while current_value < limit:
+#     current_value += number
+#     number = number + 1
+#     display += " + " + str(number)
+#
+# print(f"The consecutive sum: {display} = {current_value}")
+
+# while number <= limit:
+#     n = limit - number + 1
+#     number = (n / 2) * (number + limit)
+#
+# print(number)
