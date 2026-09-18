99e87b012da78954d3cb1f085cff2ff97d61d47e
AnasimosM <anasimosmesmesfinkebede@gmail.com>
2026-09-17 20:23:49 -0700

    Add program to print a line of hashes based on user-defined width in Part 3 of Mooc

---
+++ Mooc/Part 3/A line of hashes.py
@@ -0,0 +1,10 @@
+width = int(input("Width: "))
+
+print("#" * width)
+
+# word = "#"
+# if width > 0 and len(str(width)) > 0:
+#     print(word * width)
+#     break
+# else:
+#     print("Invalid input!!")
