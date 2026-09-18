872368f40133ca6fb12867b9a0dab6c49ed797ac
AnasimosM <anasimosmesmesfinkebede@gmail.com>
2026-09-17 21:17:12 -0700

    Add program to create a framed word with asterisks and display its length in Part 3 of Mooc

---
+++ Mooc/Part 3/A framed word.py
@@ -0,0 +1,13 @@
+word = input("Word: ")
+frame = "*" * (6 + len(word))
+middle = "*" + word.center(len(frame) - 2) + "*"
+print(frame)
+print(middle)
+print(frame)
+
+print("Length of the String is: " + str(len(word)))
+
+# frame = "*" * 30
+# print(frame)
+# print("*" + word.center(30 - 2) + "*")
+# print(frame)
