0a10a84e3fbc9f7cc89eebca2e754721f12548f3
AnasimosM <anasimosmesmesfinkebede@gmail.com>
2026-09-19 04:12:36 -0700

    Update pyramid program to display a centered pyramid with a more compact code in Part 3 of Mooc

--- Mooc/Part 3/Pyramid of #.py
+++ Mooc/Part 3/Pyramid of #.py
@@ -1,10 +1,16 @@
-block = "#"
 # place = "*"
-count = 0
+
+block = "#"
 height = int(input("Size of the Pyramid: "))
 
-while height >= count:
-    print((block + (block * count)).center(height + 1))
-    count += 2
+while height > 0:
+    print(" " * height + block)
+    block += "##"
+    height -= 1
 
-# Only displays pyramid with an odd number base.
+# count = 0
+# while height >= count:
+#     print((block + (block * count)).center(height + 1))
+#     count += 2
+#
+# # Only displays pyramid with an odd number base.
