e640435621d5615e347f95941f6414d66bd93aa3
User <asdf@gmail.com>
2026-09-22 14:30:18 +0400

    Add a program to print the first letter of each word in a sentence

---
+++ Mooc/Part 3/First letters of words.py
@@ -0,0 +1,22 @@
+# word = str(input("Please type in a sentence: "))
+# while word in word.split():
+#     print(word[0])
+
+# sentence = "Aa Bb Cc"             #str(input("Please type in a sentence: "))
+# while True:
+#     count = sentence.find(" ")
+#     print(f"Count: {count}")
+#     if count == -1:
+#         print(f"{sentence[0]}")
+#         break
+#     while count < len(sentence):
+#         print(f"{sentence[count:count+2]}")
+#         count += 1
+#     break
+#
+# count = sentence.find(" ")
+# print(f"Count: {count}")
+# print(f"Sentence: {len(sentence)} Index is {len(sentence)-1}")
+# print(f"Letter : {sentence[count]}")
+for word in input("Please type in a sentence: ").split():
+    print(word[0])
