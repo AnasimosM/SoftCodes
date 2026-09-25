d85e733049f4067a9e43c392f89ed107d8c56190
AnasimosM <322534541+AnasimosM@users.noreply.github.com>
2026-09-25 03:03:48 -0700

    Update Flip the pairs.py

--- Mooc/Part 3/Flip the pairs.py
+++ Mooc/Part 3/Flip the pairs.py
@@ -5,5 +5,15 @@ while count <= number:
         print(count)
         print(count - 1)
     count += 1
+if number % 2 != 0:
+    print(count-1)
 
 
+# index = 1
+# while index+1 <= number:
+#     print(index+1)
+#     print(index)
+#     index += 2
+ 
+# if index <= number:
+#     print(index)
