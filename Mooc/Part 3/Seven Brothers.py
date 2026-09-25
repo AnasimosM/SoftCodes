aa6765a70e5093511ee37ea0c4c982b86cc2869a
AnasimosM <322534541+AnasimosM@users.noreply.github.com>
2026-09-25 09:07:06 -0700

    Reorder names and fix sorting in seven_brothers function

--- Mooc/Part 3/Seven Brothers.py
+++ Mooc/Part 3/Seven Brothers.py
@@ -1,9 +1,11 @@
 def seven_brothers():
-  name = ["Aapo","Eero","Juhani","Lauri","Simeoni","Timo","Tuomas"]
+  name = ["Eero","Aapo","Juhani","Lauri","Simeoni","Timo","Tuomas"]
   count = 0
-  while count <= len(name)-1:
+  while count <= len(name) - 1:
+
+    name.sort()
     print(name[count])
     count += 1
-
+ 
 if __name__ == "__main__":
   seven_brothers()
