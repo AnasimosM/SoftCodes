50885637d57fe7226437cb2ceec528ae6d35059d
AnasimosM <322534541+AnasimosM@users.noreply.github.com>
2026-09-25 07:48:53 -0700

    Add function to print names of seven brothers

---
+++ Mooc/Part 3/Seven Brothers.py
@@ -0,0 +1,9 @@
+def seven_brothers():
+  name = ["Aapo","Eero","Juhani","Lauri","Simeoni","Timo","Tuomas"]
+  count = 0
+  while count <= len(name)-1:
+    print(name[count])
+    count += 1
+
+if __name__ == "__main__":
+  seven_brothers()
